f5780e1bb40df11b6355d138ae846a0ebbcfb411
Emily Costa <113480636+EmilyCosta1420@users.noreply.github.com>
2022-10-16 23:09:53 -0300

    Add files via upload
    Programa que mostra a tabuada de multiplicação do 1 até o 10.

---
+++ T2-10/src/principal/principal.java
@@ -0,0 +1,15 @@
+package principal;
+import java.util.Scanner;
+public class principal {
+
+	public static void main(String[] args) {
+		//Faça um programa que mostre as tabuadas dos números de 1 a 10.
+		System.out.println("Tabuada de Multiplicação");
+		 for (int m = 1; m <=10; m++) {
+	            for ( int multi = 1; multi <=10; multi++) {
+	            System.out.println(m +" x " + multi + " = " + m*multi);
+}
+	            System.out.println("\n");
+		 }
+	}
+}
